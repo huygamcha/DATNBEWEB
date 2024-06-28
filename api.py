9f665bc724b847af95bfe69db0f2b65fa792ea84
huygamcha <lehuynhhuy2002@gmail.com>
2024-06-28 07:40:29 +0700

    ok

--- api.py
+++ api.py
@@ -110,7 +110,70 @@ def countSevenAgo():
         # Chuyển sang ngày tiếp theo
         current_day += datetime.timedelta(days=1)
         
-    print('result', result)
     return jsonify(result)
+
+@app.route('/countForTime', methods=['GET'])
+def countForTime():
+    start_time_str = request.args.get('start')
+    select_day = request.args.get('day')
+
+    if not start_time_str:
+        return jsonify({'message': 'Yêu cầu phải đi kèm với tham số start'}), 400
+
+    try:
+        # Chuyển đổi chuỗi thời gian thành đối tượng datetime
+        start_time = datetime.datetime.fromisoformat(start_time_str)
+    except ValueError:
+        return jsonify({'message': 'Định dạng thời gian không hợp lệ. Định dạng hợp lệ: YYYY-MM-DDTHH:MM:SS'}), 400
+
+    try:
+        # Chuyển select_day thành số nguyên
+        select_day = int(select_day)
+    except ValueError:
+        return jsonify({'message': 'Ngày lựa chọn không hợp lệ. Phải là một số nguyên dương.'}), 400
+
+    # Tính end_time là 'select_day' ngày trước start_time
+    end_time = start_time - datetime.timedelta(days=select_day)
+
+    # Khởi tạo biến để lưu kết quả
+    hourly_counts = []
+
+    # Vòng lặp qua các ngày trong khoảng thời gian từ end_time đến start_time
+    current_day = end_time
+    print(current_day, start_time)
+    while current_day <= start_time:
+        # Lặp qua mỗi giờ trong ngày
+        for hour in range(24):
+            # Tính start_of_hour và end_of_hour cho mỗi giờ
+            start_of_hour = current_day.replace(hour=hour, minute=0, second=0, microsecond=0)
+            end_of_hour = current_day.replace(hour=hour, minute=59, second=59, microsecond=999999)
+
+            # Đếm số lượng tài liệu cho giờ hiện tại
+            countGoingOut = collection.count_documents({
+                'action': 'go out',
+                'time': {'$gte': start_of_hour, '$lte': end_of_hour}
+            })
+            countGoingIn = collection.count_documents({
+                'action': 'go in',
+                'time': {'$gte': start_of_hour, '$lte': end_of_hour}
+            })
+
+            # Tạo cặp khoảng thời gian và dữ liệu goingIn, goingOut
+            time_range = f"{hour:02}:00-{(hour+1) % 24:02}:00"
+            hour_data = {
+                'goingIn': countGoingIn,
+                'goingOut': countGoingOut
+            }
+
+            # Thêm vào danh sách kết quả dưới dạng mong muốn
+            hourly_counts.append([[time_range], [hour_data]])
+
+        # Chuyển sang ngày tiếp theo
+        current_day += datetime.timedelta(days=1)
+        
+    print({"hourly_counts": hourly_counts})
+    # Trả về định dạng cuối cùng cho hourly_counts
+    return jsonify({"hourly_counts": hourly_counts})
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
+
